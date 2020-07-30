6485f0798b298b54b94506795c0ab9d62fe59b2f
andriar2 <andriarmikahrun@gmail.com>
2020-07-30 08:22:52 +0800

    update codingan yang ngawur

--- palindrome-angka.php
+++ palindrome-angka.php
@@ -2,11 +2,35 @@
 
 function palindrome_angka($angka) {
   // tulis kode di sini
-}
+    $result="";
+    $str_angka=strval($angka);
+    $balikstr="";
+    for ($i = strlen($str_angka)-1; $i>= 0; $i--){
+        $balikstr.= $str_angka[$i];
+    }
+    if($balikstr==$str_angka){
+        $angka+=1;
+    }
+    while(true){
+        $str_angka=strval($angka);
+        $balikstr="";
+        for($i =strlen($str_angka)-1; $i >= 0; $i--){
+            $balikstr.=$str_angka[$i];
+        }
+        if($balikstr == $str_angka){
+            global $result;
+            $result=$str_angka."<br>";
+            return $result;
+        }
+        else{
+            $angka+=1;
+        };
+    };
+};
 
 // TEST CASES
 echo palindrome_angka(8); // 9
-echo palindrome_angka(10); // 11
+echo palindrome_angka(121); // 11
 echo palindrome_angka(117); // 121
 echo palindrome_angka(175); // 181
 echo palindrome_angka(1000); // 1001
